8acb191a3fee26750d15cd88d76173a3aa379574
ALargeBagel <aaron.tkd@hotmail.com>
2016-02-27 00:39:00 +0000

    Initial setup of project

---
+++ src/uk/tellowharton/mc/brandon/Congrats.java
@@ -0,0 +1,14 @@
+package uk.tellowharton.mc.brandon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Aaron on 27/02/2016.
+ */
+public class Congrats extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        //lel
+    }
+}
